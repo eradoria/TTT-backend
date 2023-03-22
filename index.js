2c351145e187446b633607e38108a4ce257e3172
eradoria <eradoria@gmail.com>
2023-03-21 21:51:25 -0500

    corsOptions line

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ require("dotenv").config();
 
 const db = mongoose.connection;
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const { USER, PASSWORD, PORT } = process.env;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
