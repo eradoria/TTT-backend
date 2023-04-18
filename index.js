a59f6f5d96dd31cec9612d319e3750e26fd1558d
eradoria <eradoria@gmail.com>
2023-04-17 21:17:14 -0500

    testing readd and remove

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ app.put("/update", async (req, res) => {
 });
 
 app.put("/readd", async (req, res) => {
+  console.log(req.body); // Add this line to see if the request body is being received correctly
   const id = req.body.id;
   const newRank = req.body.newRank;
   const status = req.body.status;
@@ -94,6 +95,7 @@ app.put("/readd", async (req, res) => {
 });
 
 app.put("/remove", async (req, res) => {
+  console.log(req.body); // Add this line to see if the request body is being received correctly
   const id = req.body.id;
   const newStatus = req.body.newStatus;
   console.log(id);
